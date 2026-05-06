0a51ffb8b5ac75e3610b2af22293f326c1d4fe12
raishaun cooke <thegrim5562@gmail.com>
2026-05-06 09:43:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
-//Add code below
+let quote = quotelength.length
+let quote = quote[22];
+let output = quotelength[22]
+
+document.getElementById("result").innerHTML = output
+
