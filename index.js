b5f6e9901cb2c6a89776d5175aac0e56d5489644
raishaun cooke <thegrim5562@gmail.com>
2026-05-14 10:32:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
-let quote = quotelength.length
-let quote = quote[22];
-let output = quotelength[quote-22];
+let quoteLength = quote.length
+let twentySecondLetter = quote[21];
+let output = `I love baked mac n cheese $[quoteLength] and its taste like [${twentySecondLetter}]`
 
 document.getElementById('result')innerHTML = output;
 
