e4ad5943c639595d0c465b0560716e7e426e6a5a
raishaun cooke <thegrim5562@gmail.com>
2026-05-14 10:34:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ let quoteLength = quote.length
 let twentySecondLetter = quote[21];
 let output = `I love baked mac n cheese ${quoteLength} and its taste like [${twentySecondLetter}]`
 
-document.getElementById('result')innerHTML = output;
+document.getElementById('result').innerHTML = output;
 
