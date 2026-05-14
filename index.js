7a336ce6c5ef736bd85c5cefe1e8e2b90f1cddef
raishaun cooke <thegrim5562@gmail.com>
2026-05-14 10:32:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let quote = "...ask not what your country can do for you — ask what you can do
 
 let quoteLength = quote.length
 let twentySecondLetter = quote[21];
-let output = `I love baked mac n cheese $[quoteLength] and its taste like [${twentySecondLetter}]`
+let output = `I love baked mac n cheese ${quoteLength} and its taste like [${twentySecondLetter}]`
 
 document.getElementById('result')innerHTML = output;
 
